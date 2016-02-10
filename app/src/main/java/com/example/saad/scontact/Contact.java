1d8efe05700b55e25faafe23e774bc0706787e10
saad boudfor <sboudfor@gmail.com>
2016-02-10 16:16:10 +0100

    test db et contact

--- app/src/main/java/com/example/saad/scontact/Contact.java
+++ app/src/main/java/com/example/saad/scontact/Contact.java
@@ -4,6 +4,106 @@ package com.example.saad.scontact;
  * Created by saad on 2/10/16.
  */
 public class Contact {
+    private String nom;
+    private String prenom;
+    private String phone;
+    private String portable;
 
+    private String adresse;
+    private String email;
+    private String profession;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    private int id;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String presnom) {
+        this.prenom = presnom;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPortable() {
+        return portable;
+    }
+
+    public void setPortable(String portable) {
+        this.portable = portable;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    public String getWebSite() {
+        return webSite;
+    }
+
+    public void setWebSite(String webSite) {
+        this.webSite = webSite;
+    }
+
+    public Contact(String nom, String presnom, String phone, String portable, String adresse, String email, String profession, int id, String webSite) {
+
+        this.nom = nom;
+        this.prenom = presnom;
+        this.phone = phone;
+        this.portable = portable;
+        this.adresse = adresse;
+        this.email = email;
+        this.profession = profession;
+        this.id = id;
+        this.webSite = webSite;
+    }
+
+    public Contact() {
+
+    }
+
+    private String webSite;
 
 }
--- app/src/main/java/com/example/saad/scontact/NewContact.java
+++ app/src/main/java/com/example/saad/scontact/NewContact.java
@@ -2,6 +2,9 @@ package com.example.saad.scontact;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class NewContact extends AppCompatActivity {
 
@@ -9,5 +12,46 @@ public class NewContact extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_contact);
+    }
+
+
+    public void enregisterOnClick(View view){
+        DatabaseHandler databaseHandler = new DatabaseHandler(this.getBaseContext());
+        Contact contact = new Contact();
+
+        // On construit notre objet contact:
+
+        EditText editText;
+
+        editText = (EditText) findViewById(R.id.nom1);
+        contact.setNom(editText.getText().toString());
+
+
+        editText = (EditText) findViewById(R.id.prenom1);
+        contact.setPrenom(editText.getText().toString());
+
+        editText = (EditText) findViewById(R.id.gsm1);
+        contact.setPortable(editText.getText().toString());
+
+        editText = (EditText) findViewById(R.id.tel11);
+        contact.setPhone(editText.getText().toString());
+
+
+        editText = (EditText) findViewById(R.id.adresse1);
+        contact.setAdresse(editText.getText().toString());
+
+
+        editText = (EditText) findViewById(R.id.mail1);
+        contact.setEmail(editText.getText().toString());
+
+        editText = (EditText) findViewById(R.id.proffession1);
+        contact.setProfession(editText.getText().toString());
+
+        editText = (EditText) findViewById(R.id.webSite1);
+        contact.setWebSite(editText.getText().toString());
+
+
+
+
     }
 }
